283465254bb5d60667e9dab8c9ada51d3ea926a7
brian pursell <brian@bittermelon.io>
2018-04-10 15:33:55 -0500

    commenting out css for initializing app

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,30 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Header from './components/Header.jsx';
-import styles from '../src/styles.scss';
+// import styles from '../src/styles.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      array: [0,1,2,3,4]
+      array: [0, 1, 2, 3, 4]
     };
   }
 
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <div>
-      {/* <div className="sidenav">
+        {/* <div className="sidenav">
         <a>Search</a>
       </div> */}
 
-      <button className='MagicButton'>Make Magic</button>
-      <Header data={this.state}/>
-
+        <button className="MagicButton">Make Magic</button>
+        <Header data={this.state} />
       </div>
     );
   }
