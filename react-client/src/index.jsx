97e4debfc87d26091bb7e414171087b41e3dac85
JoshuaBrigati <brigati.joshua@gmail.com>
2018-04-10 11:06:19 -0500

    basic front end

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Header from './components/Header.jsx';
+import styles from '../src/styles.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      array: [0,1,2,3,4]
+    };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+
+  }
 
   render() {
     return (
       <div>
-        <Header name={'Hello world!'} />
+      {/* <div className="sidenav">
+        <a>Search</a>
+      </div> */}
+
+      <button className='MagicButton'>Make Magic</button>
+      <Header data={this.state}/>
+
       </div>
     );
   }
