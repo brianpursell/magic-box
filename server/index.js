08cf4e83dfc0ff29c43728bd876dccf7f32cb54a
brian pursell <brian@bittermelon.io>
2018-04-10 15:45:12 -0500

    added .env

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
@@ -7,6 +8,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('listening on port ' + port + '!');
 });
