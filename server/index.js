2ecc4f3f22dd3756db528edf802280388840a150
brian pursell <brian@bittermelon.io>
2018-04-10 16:12:58 -0500

    serving root

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
+app.get('/', (req, res) => {
+  res.send('welcome');
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('listening on port ' + port + '!');
