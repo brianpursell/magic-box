63db8ff44b383469b4623fa044f3b6786b00f527
Cole Packingham <cpackingham@Coles-MacBook-Pro.local>
2018-04-15 17:59:51 -0500

    Added semantic ui, icons, routing

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import Routes from './Routes.jsx';
 import NavBar from './NavBar.jsx';
+import Login from './Login.jsx';
 import './styles.scss';
 
 class App extends Component {
   state = {
-
+    authenticated: true
   }
 
   render = () => {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='one column sidenav'>
-            <NavBar />
-          </div>
-          <div className='eleven columns'>
-            <Routes />
-          </div>
-        </div>
+      <div className='full height app'>
+        <NavBar />
+        {this.state.authenticated ? <Routes /> : <Login />}
       </div>
     );
   }
--- web/src/NavBar.jsx
+++ web/src/NavBar.jsx
@@ -7,27 +7,36 @@ class NavBar extends Component {
     selectedRoute: paths.HOME
   }
   
-  onClick = (path) => this.setState({selectedRoute: path});
+  onClick = (path) => {
+    this.setState({selectedRoute: path});
+  }
   
   render = () => {
-    console.log(Object.keys(paths))
     return (
-      <React.Fragment>
-      {Object.entries(paths).reduce((buttons, [key, route]) => {
-        console.log(`key: ${key}, route: ${route}`)
-        buttons.push(<NavButton onClick={this.onClick.bind(this)} key={key} route={route} />)
-        return buttons;
-      }, [])}
-      </React.Fragment>
+      <div className='ui sticky inverted vertical menu'>
+        <div className='item' style={{backgroundColor: 'maroon', display: 'block'}}>
+          <img src='assets/logo.svg' style={{height: 70, width: 70}}/>
+          EMOTIONAL JAMSEL
+        </div>
+        {
+          Object.entries(paths).reduce((buttons, [name, route]) => {
+            buttons.push(<NavButton onClick={this.onClick.bind(this)} key={name} name={name} route={route} />)
+            return buttons;
+          }, [])
+        }
+      </div>
     );
   }
 }
 
-const NavButton = ({ onClick, key, route }) => (
-  <button onClick={() => onClick(route)}>
-    <Link to={route} />
-    {key.toLowerCase()}
-  </button>
-);
+const NavButton = ({ onClick, name, route }) => {
+  const iconPath = `assets/${name.toLowerCase()}.svg`;
+  return (
+    <Link className='item' to={route}> 
+      <img src={iconPath} style={{height: 100, width: 100}} />
+      {name.toLowerCase()}
+    </Link>
+  );
+};
 
 export default NavBar;
