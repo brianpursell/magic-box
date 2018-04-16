5d443b1f8e639b3d8014490b3f55a30b7520004e
justscribner <justscribner@gmail.com>
2018-04-16 16:15:22 -0500

    votes for music almost done waiting on josh to get the buttons up. Working on docker hot reload now

--- web/index.js
+++ web/index.js
@@ -10,49 +10,51 @@ const app = express();
 app.use(express.static(`${__dirname}/dist`));
 
 app.get('/home', (req, res) => {
-  db.load(data => {
+  db.load((data) => {
     res.send(data.rows);
   });
 });
 
 app.get('/users', (req, res) => {
-  db.users(data => {
+  db.users((data) => {
     res.send(data.rows);
   });
 });
 
 app.post('/signup', (req, res) => {
   console.log(req.body);
-  db.signup(req.body, response => {
+  db.signup(req.body, (response) => {
     res.send(req.body);
   });
 });
 
-app.get('*', function(req, res) {
-  res.send('what???', 404);
+app.get(['/music', '/home', '/prompts', '/sprites', '/worlds'], (req, res) => {
+  // res.send('what???', 404);
   console.log('called');
-  db.load(data => {
+  db.load((data) => {
     console.log('data: ', data);
     res.send(data);
   });
 });
 
-app.get('*', (req, res) => {
-  res.send('what???', 404);
-});
+// app.get('*', (req, res) => {
+//   res.send('what???', 404);
+// });
 
 app.get('/votes', (req, res) => {
-  db.didVote(10, 1000, data => {
-    res.send(data.rows);
+  db.didVote(10, 1000, (data) => {
+    console.log('these are the data.rows => ', data.rows);
+    res.send('these are the data.rows => ', data.rows);
   });
 });
 
 app.post('/votes', (req, res) => {
   console.log('I am the req.body => ', req.body);
-  // db.toggleVote(song, vote, (data) => {
-  //   res.send(data);
-  // });
-  res.send();
+  db.toggleVote(song, vote, (data) => {
+    console.log('Vote Toggled!');
+    console.log('this is the data that got to the server from the votes post req => ', data);
+  });
+  res.send(data);
 });
 
 const port = process.env.PORT || 3000;
