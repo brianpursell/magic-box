c2677550a87e4fc028598428e7fdafe83ebcc194
justscribner <justscribner@gmail.com>
2018-04-21 10:38:13 -0500

    commiting before merge of newest changed

--- web/db/index.js
+++ web/db/index.js
@@ -9,35 +9,32 @@ const client = new pg.Client();
 client
   .connect()
   .then(() => {
-    console.log(
-      `Connected To ${client.database} at ${client.host}:${client.port}`
-    );
+    console.log(`Connected To ${client.database} at ${client.host}:${client.port}`);
   })
   .catch(err => console.error(err));
 
 // get for homepage
-const getSongs = callback => {
+const getSongs = (callback) => {
   client
     .query('SELECT * FROM songs ORDER BY id')
-    .then(data => {
+    .then((data) => {
       callback(data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
     });
 };
 
-const altGetSongs = callback => {
+const altGetSongs = (callback) => {
   client
     .query(
       // 'SELECT * FROM votes'
-      'SELECT *, (SELECT COUNT(*) FROM alt_votes WHERE alt_votes.song_id = alt_songs.id AND alt_votes.vote = true) AS up, (SELECT COUNT(*) FROM alt_votes WHERE alt_votes.song_id = alt_songs.id AND alt_votes.vote = false) AS down FROM alt_songs ORDER BY alt_songs.id'
-    )
-    .then(data => {
+      'SELECT *, (SELECT COUNT(*) FROM alt_votes WHERE alt_votes.song_id = alt_songs.id AND alt_votes.vote = true) AS up, (SELECT COUNT(*) FROM alt_votes WHERE alt_votes.song_id = alt_songs.id AND alt_votes.vote = false) AS down FROM alt_songs ORDER BY alt_songs.id')
+    .then((data) => {
       console.log(data);
       callback(data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
     });
 };
@@ -77,10 +74,10 @@ const loadWorlds = (callback) => {
 const findById = (id, callback) => {
   client
     .query(`SELECT * FROM users WHERE id = ${id}`)
-    .then(data => {
+    .then((data) => {
       callback(null, data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err, null);
     });
 };
@@ -88,35 +85,33 @@ const findById = (id, callback) => {
 const findByUsername = (username, callback) => {
   client
     .query(`SELECT * FROM users WHERE username = '${username}';`)
-    .then(data => {
+    .then((data) => {
       callback(null, data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log('err: ', err);
       callback(err, null);
     });
 };
 
-const users = callback => {
+const users = (callback) => {
   client
     .query('SELECT * FROM users')
-    .then(data => {
+    .then((data) => {
       callback(data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
     });
 };
 
 const signup = (username, password, callback) => {
   client
-    .query(
-      `INSERT INTO users (username, password) VALUES('${username}', '${password}');`
-    )
-    .then(success => {
+    .query(`INSERT INTO users (username, password) VALUES('${username}', '${password}');`)
+    .then((success) => {
       callback(null, true);
     })
-    .catch(err => {
+    .catch((err) => {
       callback(err, null);
     });
 };
@@ -124,10 +119,10 @@ const signup = (username, password, callback) => {
 const updateVotesQuery = (params, voteId, callback) => {
   client
     .query(`${params} where id = ${voteId};`)
-    .then(data => {
+    .then((data) => {
       callback(data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
     });
 };
@@ -135,10 +130,10 @@ const updateVotesQuery = (params, voteId, callback) => {
 const updateSongVotesQuery = (params, songId, callback) => {
   client
     .query(`${params} where id = ${songId};`)
-    .then(data => {
+    .then((data) => {
       callback(data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
     });
 };
@@ -157,80 +152,46 @@ const toggleVote = (vote, callback) => {
   if (voteType === 'upvote') {
     if (totalVotes === 1 && upVoteCount === 1) {
       updateVotesQuery('update votes set upvote = 0', voteId, callback);
-      updateSongVotesQuery(
-        'update songs set upvotes = upvotes - 1',
-        songId,
-        callback
-      );
+      updateSongVotesQuery('update songs set upvotes = upvotes - 1', songId, callback);
     } else if (totalVotes === 1 && upVoteCount === 0) {
       updateVotesQuery(
         'update votes set upvote = (case upvote when 1 then 0 when 0 then 1 else upvote end), downvote = (case downvote when 1 then 0 when 0 then 1 else downvote end)',
         voteId,
-        callback
+        callback,
       );
-      updateSongVotesQuery(
-        'update songs set upvotes = upvotes + 1',
-        songId,
-        callback
-      );
-      updateSongVotesQuery(
-        'update songs set downvotes = downvotes - 1',
-        songId,
-        callback
-      );
+      updateSongVotesQuery('update songs set upvotes = upvotes + 1', songId, callback);
+      updateSongVotesQuery('update songs set downvotes = downvotes - 1', songId, callback);
     } else if (totalVotes === 0) {
       updateVotesQuery('update votes set upvote = 1', voteId, callback);
-      updateSongVotesQuery(
-        'update songs set upvotes = upvotes + 1',
-        songId,
-        callback
-      );
+      updateSongVotesQuery('update songs set upvotes = upvotes + 1', songId, callback);
     }
   } else if (voteType === 'downvote') {
     if (totalVotes === 1 && downVoteCount === 1) {
       updateVotesQuery('update votes set downvote = 0', voteId, callback);
-      updateSongVotesQuery(
-        'update songs set downvotes = downvotes - 1',
-        songId,
-        callback
-      );
+      updateSongVotesQuery('update songs set downvotes = downvotes - 1', songId, callback);
     } else if (totalVotes === 1 && downVoteCount === 0) {
       updateVotesQuery(
         'update votes set upvote = (case upvote when 1 then 0 when 0 then 1 else upvote end), downvote = (case downvote when 1 then 0 when 0 then 1 else downvote end)',
         voteId,
-        callback
+        callback,
       );
-      updateSongVotesQuery(
-        'update songs set downvotes = downvotes + 1',
-        songId,
-        callback
-      );
-      updateSongVotesQuery(
-        'update songs set upvotes = upvotes - 1',
-        songId,
-        callback
-      );
+      updateSongVotesQuery('update songs set downvotes = downvotes + 1', songId, callback);
+      updateSongVotesQuery('update songs set upvotes = upvotes - 1', songId, callback);
     } else if (totalVotes === 0) {
       updateVotesQuery('update votes set downvote = 1', voteId, callback);
-      updateSongVotesQuery(
-        'update songs set downvotes = downvotes + 1',
-        songId,
-        callback
-      );
+      updateSongVotesQuery('update songs set downvotes = downvotes + 1', songId, callback);
     }
   }
 };
 
 const didVote = (currentUserId, clickedSongId, callback) => {
   client
-    .query(
-      `Select * from votes where votes.user_id = ${currentUserId} and votes.song_id = ${clickedSongId};`
-    )
-    .then(data => {
+    .query(`Select * from votes where votes.user_id = ${currentUserId} and votes.song_id = ${clickedSongId};`)
+    .then((data) => {
       console.log('data => ', data);
       callback(data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
     });
 };
